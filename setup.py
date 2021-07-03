4ab69e37be4e0369f2dc164a5e313fcab4a74e9e
Matt Spurlin <mattsp1290@gmail.com>
2021-07-03 12:19:06 -0400

    add separate get_payload func

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='flask_auth0',
-    version='0.5',
+    version='0.6',
     py_modules=['flask_auth0'],
     packages=find_packages(),
     install_requires=[
