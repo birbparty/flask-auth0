dc9dad4091b4d35bd4ac31183fc2b44e1a856e8d
Matt Spurlin <mattsp1290@gmail.com>
2021-07-29 21:46:19 -0400

    pypi registration

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='flask_auth0',
+    name='birb_flask_auth0',
     version='0.7',
-    py_modules=['flask_auth0'],
+    py_modules=['birb_flask_auth0'],
     packages=find_packages(),
     install_requires=[
         'Flask==1.1.2'
